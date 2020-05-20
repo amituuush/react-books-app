7ef407d3eec8da741dcb43d06573138a07c9e86e
Naila Ahmad <naila@nyu.edu>
2020-05-20 12:29:37 -0400

    adding base html

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,41 @@
 import React from 'react'
 
 const App = () => (
-  <div>
-    Hello there!
+  <div class="wrapper">
+
+    <div className="add">
+      <input type="text" placeholder="Title" />
+      <input type="number" placeholder="Cost" />
+      <input type="number" placeholder="Quantity" />
+      <button>+</button>
+    </div>
+
+    <div className="headers">
+      <span>Title</span>
+      <span>Cost</span>
+      <span>Quantity</span>
+    </div>
+
+    <div className="books">
+      <div className="book">
+        <span>Tom Sawyer</span>
+        <span>$10</span>
+        <span>2</span>
+        <button>-</button>
+      </div>
+      <div className="book">
+        <span>The Old Man and the Sea</span>
+        <span>$5</span>
+        <span>1</span>
+        <button>-</button>
+      </div>
+    </div>
+
+    <div className="totals">
+      <span>$25</span>
+      <span>3</span>
+    </div>
+
   </div>
 )
 
